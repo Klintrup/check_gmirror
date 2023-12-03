95cfc1995e095c9ef9004685c35f24b54b58e68a
Søren Klintrup <soren@klintrup.dk>
2023-12-03 20:06:34 +0100

    update shellscript

--- check_gmirror.sh
+++ check_gmirror.sh
@@ -5,6 +5,7 @@
 PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 if [ -x "/sbin/gmirror" ]
 then
+ # shellcheck disable=SC2086
  DEVICES="$(gmirror list|grep "Geom name"|sed -Ee 's/Geom name: //')"
 else
  ERRORSTRING="gmirror binary does not exist on system"
@@ -17,17 +18,19 @@ unset ERR
 
 for DEVICE in ${DEVICES}
 do
+ # shellcheck disable=SC2086
  DEVICESTRING="$(gmirror list ${DEVICE}|grep -e "^State"|sed -e 's/State: //')"
- if [ "$(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]
+ # shellcheck disable=SC2086
+ if [ "$(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]
  then
   ERRORSTRING="${ERRORSTRING} / ${DEVICE}: unknown state"
   if ! [ "${ERR}" = 2 ];then ERR=3;fi
  else
-  case $(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
+  case $(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
    degraded)
     if [ $(gmirror list ${DEVICE}|grep "^   Flags"|grep -q "SYNCHRONIZING") ]
     then
-     if ! [ "${ERR}" = 2 -o "${ERR}" = 3 ]; then ERR=1;fi
+     if ! [ "${ERR}" = 2 ] || [ "${ERR}" = 3 ]; then ERR=1;fi
      ERRORSTRING="${ERRORSTRING} / ${DEVICE}: rebuilding"
     else
      ERR=2
@@ -49,12 +52,12 @@ if [ "${1}" ]
 then
  if [ "${ERRORSTRING}" ]
  then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E ${*}
+  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E "${@}"
  fi
 else
- if [ "${ERRORSTRING}" -o "${OKSTRING}" ]
+ if [ "${ERRORSTRING}" ] || [ "${OKSTRING}" ]
  then
-  echo ${ERRORSTRING} ${OKSTRING}|sed s/"^\/ "//
+  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//
   exit ${ERR}
  else
   echo no raid volumes found
