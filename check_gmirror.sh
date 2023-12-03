d80ce0aefc3512cd82dd6dd11d037431ceb073d7
Søren Klintrup <soren@klintrup.dk>
2023-12-03 20:11:08 +0100

    revert to 1.1 (with known errors)

--- check_gmirror.sh
+++ check_gmirror.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 # NRPE check for gmirror
-# Written by: SÃ¸ren Klintrup <github at klintrup.dk>
-
-unset ERRORSTRING OKSTRING ERR DEVICES DEVICE DEVICESTRING
+# Written by: Søren Klintrup <soren at klintrup.dk>
+# Get your copy from http://soren.klintrup.dk/gmirror/
+# version 1.1
 
 PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 if [ -x "/sbin/gmirror" ]
 then
- # shellcheck disable=SC2086
  DEVICES="$(gmirror list|grep "Geom name"|sed -Ee 's/Geom name: //')"
 else
  ERRORSTRING="gmirror binary does not exist on system"
  ERR=3
 fi
 
+unset ERRORSTRING
+unset OKSTRING
+unset ERR
+
 for DEVICE in ${DEVICES}
 do
- # shellcheck disable=SC2086
  DEVICESTRING="$(gmirror list ${DEVICE}|grep -e "^State"|sed -e 's/State: //')"
- # shellcheck disable=SC2086
- if [ "$(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]
+ if [ "$(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]
  then
   ERRORSTRING="${ERRORSTRING} / ${DEVICE}: unknown state"
   if ! [ "${ERR}" = 2 ];then ERR=3;fi
  else
-  case $(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
+  case $(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
    degraded)
-    # shellcheck disable=SC2143
-    if [ "$(gmirror list "${DEVICE}"|grep "^   Flags"|grep -q "SYNCHRONIZING")" ]
+    if [ $(gmirror list ${DEVICE}|grep "^   Flags"|grep -q "SYNCHRONIZING") ]
     then
-     if ! [ "${ERR}" = 2 ] || [ "${ERR}" = 3 ]; then ERR=1;fi
+     if ! [ "${ERR}" = 2 -o "${ERR}" = 3 ]; then ERR=1;fi
      ERRORSTRING="${ERRORSTRING} / ${DEVICE}: rebuilding"
     else
      ERR=2
@@ -51,12 +51,12 @@ if [ "${1}" ]
 then
  if [ "${ERRORSTRING}" ]
  then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E "${@}"
+  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E ${*}
  fi
 else
- if [ "${ERRORSTRING}" ] || [ "${OKSTRING}" ]
+ if [ "${ERRORSTRING}" -o "${OKSTRING}" ]
  then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//
+  echo ${ERRORSTRING} ${OKSTRING}|sed s/"^\/ "//
   exit ${ERR}
  else
   echo no raid volumes found
