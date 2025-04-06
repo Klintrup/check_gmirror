ab40f3bb1a2d1366d598e923215c9ab5ecf77a8f
Søren Klintrup <soren@klintrup.dk>
2025-04-06 21:33:32 +0200

    Add MegaLinter workflow for automated code quality checks (#23)
    This pull request includes several changes to improve the functionality
    and maintainability of the codebase, specifically in the CI/CD workflows
    and the `check_gmirror.sh` script. The most important changes include
    adding a MegaLinter workflow, updating permissions for the SonarCloud
    workflow, and refactoring the `check_gmirror.sh` script for better
    readability and performance.
    
    ### CI/CD Workflow Improvements:
    * Added a new MegaLinter workflow in `.github/workflows/megalinter.yml`
    to automate code linting and fixing. This workflow runs on pull requests
    targeting the `master` and `main` branches and includes configurations
    for applying fixes automatically.
    * Updated the SonarCloud workflow in `.github/workflows/sonarcloud.yml`
    to include necessary permissions for SonarQube to read code, create
    issues, and upload results.
    
    ### Script Refactoring:
    * Refactored the `check_gmirror.sh` script to improve readability by
    combining single-line `if` statements with their corresponding `then`
    statements. This change simplifies the script and makes it easier to
    follow.
    [[1]](diffhunk://#diff-6fe28a0e0819eb06e45f4066636701348a5266cd1e86c9bfb8d8e97eb23360d5L8-R27)
    [[2]](diffhunk://#diff-6fe28a0e0819eb06e45f4066636701348a5266cd1e86c9bfb8d8e97eb23360d5L50-R51)
    
    ---------
    
    Co-authored-by: Klintrup <18490009+Klintrup@users.noreply.github.com>
    Co-authored-by: Klintrup <Klintrup@users.noreply.github.com>

--- check_gmirror.sh
+++ check_gmirror.sh
@@ -5,61 +5,54 @@
 unset ERRORSTRING OKSTRING ERR DEVICES DEVICE DEVICESTRING
 
 PATH="/sbin:/bin:/usr/sbin:/usr/bin"
-if [ -x "/sbin/gmirror" ]
-then
- # shellcheck disable=SC2086
- DEVICES="$(gmirror list|grep "Geom name"|sed -Ee 's/Geom name: //')"
+if [ -x "/sbin/gmirror" ]; then
+	# shellcheck disable=SC2086
+	DEVICES="$(gmirror list | grep "Geom name" | sed -Ee 's/Geom name: //')"
 else
- ERRORSTRING="gmirror binary does not exist on system"
- ERR=3
+	ERRORSTRING="gmirror binary does not exist on system"
+	ERR=3
 fi
 
-for DEVICE in ${DEVICES}
-do
- # shellcheck disable=SC2086
- DEVICESTRING="$(gmirror list ${DEVICE}|grep -e "^State"|sed -e 's/State: //')"
- # shellcheck disable=SC2086
- if [ "$(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]
- then
-  ERRORSTRING="${ERRORSTRING} / ${DEVICE}: unknown state"
-  if ! [ "${ERR}" = 2 ];then ERR=3;fi
- else
-  case $(echo ${DEVICESTRING}|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
-   degraded)
-    # shellcheck disable=SC2143
-    if [ "$(gmirror list "${DEVICE}"|grep "^   Flags"|grep -q "SYNCHRONIZING")" ]
-    then
-     if ! [ "${ERR}" = 2 ] || [ "${ERR}" = 3 ]; then ERR=1;fi
-     ERRORSTRING="${ERRORSTRING} / ${DEVICE}: rebuilding"
-    else
-     ERR=2
-     ERRORSTRING="${ERRORSTRING} / ${DEVICE}: DEGRADED"
-    fi
-    ;;
-   starting)
-    ERR=3
-    ERRORSTRING="${ERRORSTRING} / ${DEVICE}: starting"
-    ;;
-   complete)
-    OKSTRING="${OKSTRING} / ${DEVICE}: ok"
-    ;;
-   esac
- fi
+for DEVICE in ${DEVICES}; do
+	# shellcheck disable=SC2086
+	DEVICESTRING="$(gmirror list ${DEVICE} | grep -e "^State" | sed -e 's/State: //')"
+	# shellcheck disable=SC2086
+	if [ "$(echo ${DEVICESTRING} | tr '[:upper:]' '[:lower:]' | sed -Ee 's/.*(complete|degraded|starting).*/\1/')" = "" ]; then
+		ERRORSTRING="${ERRORSTRING} / ${DEVICE}: unknown state"
+		if ! [ "${ERR}" = 2 ]; then ERR=3; fi
+	else
+		case $(echo ${DEVICESTRING} | tr '[:upper:]' '[:lower:]' | sed -Ee 's/.*(complete|degraded|starting).*/\1/') in
+		degraded)
+			# shellcheck disable=SC2143
+			if [ "$(gmirror list "${DEVICE}" | grep "^   Flags" | grep -q "SYNCHRONIZING")" ]; then
+				if ! [ "${ERR}" = 2 ] || [ "${ERR}" = 3 ]; then ERR=1; fi
+				ERRORSTRING="${ERRORSTRING} / ${DEVICE}: rebuilding"
+			else
+				ERR=2
+				ERRORSTRING="${ERRORSTRING} / ${DEVICE}: DEGRADED"
+			fi
+			;;
+		starting)
+			ERR=3
+			ERRORSTRING="${ERRORSTRING} / ${DEVICE}: starting"
+			;;
+		complete)
+			OKSTRING="${OKSTRING} / ${DEVICE}: ok"
+			;;
+		esac
+	fi
 done
 
-if [ "${1}" ]
-then
- if [ "${ERRORSTRING}" ]
- then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E "${@}"
- fi
+if [ "${1}" ]; then
+	if [ "${ERRORSTRING}" ]; then
+		echo "${ERRORSTRING} ${OKSTRING}" | sed s/"^\/ "// | mail -s "$(hostname -s): ${0} reports errors" -E "${@}"
+	fi
 else
- if [ "${ERRORSTRING}" ] || [ "${OKSTRING}" ]
- then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//
-  exit ${ERR}
- else
-  echo no raid volumes found
-  exit 3
- fi
+	if [ "${ERRORSTRING}" ] || [ "${OKSTRING}" ]; then
+		echo "${ERRORSTRING} ${OKSTRING}" | sed s/"^\/ "//
+		exit ${ERR}
+	else
+		echo no raid volumes found
+		exit 3
+	fi
 fi
